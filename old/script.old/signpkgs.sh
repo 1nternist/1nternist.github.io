af363bbbd61628a6b5d17acd5658dfc5b0e41c5e
1nternist <dc1nternist@icloud.com>
2016-02-11 01:33:59 -0600

    Repository update

--- old/script.old/signpkgs.sh
+++ old/script.old/signpkgs.sh
@@ -2,10 +2,10 @@
 
 rm -rf Release.gpg
 
-echo "Origin: 1nternist.github.io" > Release
+echo "Origin: 1nternist Repo" > Release
 echo "Label: 1nternist.github.io" >> Release
 echo "Suite: stable" >> Release
-echo "Version: 1.2" >> Release
+echo "Version: 1.0" >> Release
 echo "Codename: dc1nternist" >> Release
 echo "Architectures: iphoneos-arm" >> Release
 echo "Components: main" >> Release
