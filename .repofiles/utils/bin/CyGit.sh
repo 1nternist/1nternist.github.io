512bdc344332bf58a93ed4685983f99e18834789
1nternist <dc1nternist@icloud.com>
2018-07-29 15:39:15 -0500

    Repository update 07-29-2018 15:39:03

--- .repofiles/utils/bin/CyGit.sh
+++ .repofiles/utils/bin/CyGit.sh
@@ -151,10 +151,10 @@ echo "MD5Sum:" >>Release
 echo " `md5sum Packages | cut -d ' ' -f1` `stat --format=%s Packages` Packages" >>Release
 echo " `md5sum Packages.bz2 | cut -d ' ' -f1` `stat --format=%s Packages.bz2` Packages.bz2" >>Release
 echo " `md5sum Packages.gz | cut -d ' ' -f1` `stat --format=%s Packages.gz` Packages.gz" >>Release
-#echo "SHA1:" >>Release
-#echo " `sha1sum Packages | cut -d ' ' -f1` `stat --format=%s Packages` Packages" >>Release
-#echo " `sha1sum Packages.bz2 | cut -d ' ' -f1` `stat --format=%s Packages.bz2` Packages.bz2" >>Release
-#echo " `sha1sum Packages.gz | cut -d ' ' -f1` `stat --format=%s Packages.gz` Packages.gz" >>Release
+echo "SHA1:" >>Release
+echo " `sha1sum Packages | cut -d ' ' -f1` `stat --format=%s Packages` Packages" >>Release
+echo " `sha1sum Packages.bz2 | cut -d ' ' -f1` `stat --format=%s Packages.bz2` Packages.bz2" >>Release
+echo " `sha1sum Packages.gz | cut -d ' ' -f1` `stat --format=%s Packages.gz` Packages.gz" >>Release
 echo "SHA256:" >>Release
 echo " `sha256sum Packages | cut -d ' ' -f1` `stat --format=%s Packages` Packages" >>Release
 echo " `sha256sum Packages.bz2 | cut -d ' ' -f1` `stat --format=%s Packages.bz2` Packages.bz2" >>Release
@@ -163,7 +163,7 @@ echo "SHA512:" >>Release
 echo " `sha512sum Packages | cut -d ' ' -f1` `stat --format=%s Packages` Packages" >>Release
 echo " `sha512sum Packages.bz2 | cut -d ' ' -f1` `stat --format=%s Packages.bz2` Packages.bz2" >>Release
 echo " `sha512sum Packages.gz | cut -d ' ' -f1` `stat --format=%s Packages.gz` Packages.gz" >>Release
-gpg --passphrase-file /usr/share/keyrings/passwd/github --batch -abs --digest-algo SHA256 -u dc1nternist -o Release.gpg Release
+gpg --passphrase-file /usr/share/keyrings/passwd/github --batch -abs -u dc1nternist -o Release.gpg Release
 echo " ➔ Release File Signed Successfully."
 sleep 2
 }
